ddf8a183943dc98fa9e628cb9ccb49c7d5df2078
Darren Gegantino <darrengegantino@gmail.com>
2018-12-21 01:23:03 +0800

    create reusable components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="img" src={props.img} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 8:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Hello!</div>
+        <div className="text">
+          {props.content}
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Darren" />
-      <CommentDetail author="Shaye" />
-      <CommentDetail author="Meadow" />
-    </div>
-  );
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          img={faker.image.avatar()}
+          timeAgo="Today at 6:00PM"
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          img={faker.image.avatar()}
+          timeAgo="Today at 5:59PM"
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+    </div>  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
